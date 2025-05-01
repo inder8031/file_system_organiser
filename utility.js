6fa735223779699f2843c43e7d3c4af907dd089f
unknown <inderece8031@gmail.com>
2025-05-01 18:00:20 +0530

    utilities

---
+++ utility.js
@@ -0,0 +1,10 @@
+let utility = {};
+
+utility.types = {
+    media: ['mp4', 'mkv', 'jpg', 'jpeg', 'png'],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', 'xz'],
+    documents: ['docx', 'doc', 'pdf', 'sql', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex', 'pptx', 'srt'],
+    app: ['exe', 'dmg', 'pkg', 'deb']
+}
+
+module.exports = utility;
